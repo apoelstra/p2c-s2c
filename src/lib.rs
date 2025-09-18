b5945d2547799a97e742340bde86478ce998521b
Andrew Poelstra <apoelstra@wpsoftware.net>
2025-09-18 21:19:33 +0000

    get ECDSA signing and verification working

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,11 @@
 //! Pay-to-Contract Sign-to-Contract
 
 mod hashes;
+mod signature;
 
 pub use hashes::{Pay2ContractHash, Sign2ContractHash, TweakHash as _};
+
+use crate::signature::NonceFnData;
 
 #[derive(Copy, Clone, PartialEq, Eq, Hash, Debug)]
 pub struct TweakedPublicKey {
@@ -34,6 +37,28 @@ impl TweakedPublicKey {
     ) -> bool {
         *self == Self::new(secp, untweaked_key, algo, data)
     }
+
+    pub fn verify_ecdsa_commitment<C: secp256k1::Verification>(
+        &self,
+        secp: &secp256k1::Secp256k1<C>,
+        sig: &secp256k1::ecdsa::Signature,
+        untweaked_nonce: &secp256k1::PublicKey,
+        algo: &str,
+        data: &[u8],
+    ) -> bool {
+        let sig_ser = sig.serialize_compact();
+
+        let tweak_bytes = crate::Sign2ContractHash::compute_tweak(&untweaked_nonce, algo, data).to_byte_array();
+        let tweak_sc = secp256k1::Scalar::from_be_bytes(tweak_bytes).expect("cryptographically unreachable");
+        let tweaked_nonce = untweaked_nonce.add_exp_tweak(secp, &tweak_sc).expect("cryptographically unreachable");
+        let nonce_ser = tweaked_nonce.serialize();
+
+        &nonce_ser[1..] == &sig_ser[..32]
+    }
+
+    pub fn as_public_key(&self) -> &secp256k1::PublicKey {
+        &self.inner
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -56,15 +81,50 @@ impl TweakedKeypair {
 
         let tweak_bytes = Pay2ContractHash::compute_tweak(&untweaked_pubkey, algo, data).to_byte_array();
         let tweak_sc = secp256k1::Scalar::from_be_bytes(tweak_bytes).expect("cryptographically unreachable");
-        Self {
-            seckey: untweaked_key.add_tweak(&tweak_sc).expect("cryptographically unreachable"),
-            pubkey: untweaked_pubkey.add_exp_tweak(&secp, &tweak_sc).expect("cryptographically unreachable"),
-        }
+        let seckey = untweaked_key.add_tweak(&tweak_sc).expect("cryptographically unreachable");
+        let pubkey = untweaked_pubkey.add_exp_tweak(&secp, &tweak_sc).expect("cryptographically unreachable");
+        debug_assert_eq!(
+            seckey.public_key(secp),
+            pubkey,
+        );
+        Self { seckey, pubkey }
     }
 
     pub fn public_key(&self) -> TweakedPublicKey {
         TweakedPublicKey {
             inner: self.pubkey,
+        }
+    }
+
+    pub fn sign_ecdsa<C: secp256k1::Signing>(
+        &self,
+        secp: &secp256k1::Secp256k1<C>,
+        msg: impl Into<secp256k1::Message>,
+        s2c_algo: &str,
+        s2c_data: &[u8],
+    ) -> (secp256k1::ecdsa::Signature, secp256k1::PublicKey) {
+        use secp256k1::ffi::CPtr as _;
+        
+        let msg = msg.into();
+        let mut data = NonceFnData {
+            ctx: secp,
+            algo: s2c_algo,
+            data: s2c_data,
+            original_nonce: None,
+        };
+
+        unsafe {
+            // SAFETY: ffi call; all parameters chosen correctly
+            let mut ret = secp256k1::ffi::Signature::new();
+            assert!(secp256k1::ffi::secp256k1_ecdsa_sign(
+                secp.ctx().as_ptr().cast_const(),
+                &mut ret,
+                msg.as_c_ptr(),
+                self.seckey.as_c_ptr(),
+                Some(crate::signature::s2c_ecdsa_nonce_fn::<C>),
+                (&mut data as *mut NonceFnData<C>).cast::<core::ffi::c_void>(),
+            ) == 1);
+            (ret.into(), data.original_nonce.unwrap())
         }
     }
 }
@@ -74,6 +134,7 @@ mod tests {
     use super::*;
 
     const TEST_ALGO: &str = "TestAlgorithm/v001";
+    const TEST_ALGO2: &str = "TestAlgorithm/v002";
     
     #[test]
     fn end_to_end_sign() {
@@ -95,6 +156,32 @@ mod tests {
 
         assert!(tweaked_pk.verify_commitment(&secp, &keypair.public_key(), TEST_ALGO, prog));
 
+        let prog2 = b"this is NOT a thing I'm committing in the pubkey";
+        let tweaked_keypair_3 = TweakedKeypair::new(&secp, &sk, None, TEST_ALGO, prog2);
+        assert_ne!(tweaked_keypair_1, tweaked_keypair_3);
+        let tweaked_keypair_3 = TweakedKeypair::new(&secp, &sk, None, TEST_ALGO2, prog);
+        assert_ne!(tweaked_keypair_1, tweaked_keypair_3);
+        let tweaked_keypair_3 = TweakedKeypair::new(&secp, &sk, None, TEST_ALGO2, prog2);
+        assert_ne!(tweaked_keypair_1, tweaked_keypair_3);
+
         // Tweaked signature
+        let wit = b"this is a thing I'm committing in the signature";
+        let msg = secp256k1::Message::from_digest(*b"the sentence is a digest I swear");
+        let (sig, nonce) = tweaked_keypair_1.sign_ecdsa(
+            &secp,
+            msg,
+            TEST_ALGO,
+            wit,
+        );
+
+        // Signature verifies as a normal signature
+        secp.verify_ecdsa(msg, &sig, &tweaked_pk.as_public_key()).unwrap();
+
+        // With original nonce, can also verify commitment
+        assert!(tweaked_pk.verify_ecdsa_commitment(&secp, &sig, &nonce, TEST_ALGO, wit));
+        assert!(!tweaked_pk.verify_ecdsa_commitment(&secp, &sig, &nonce, TEST_ALGO2, wit));
+        let wit2 = b"this is NOT a thing I'm committing in the signature";
+        assert!(!tweaked_pk.verify_ecdsa_commitment(&secp, &sig, &nonce, TEST_ALGO, wit2));
+        assert!(!tweaked_pk.verify_ecdsa_commitment(&secp, &sig, &nonce, TEST_ALGO2, wit2));
     }
 }
---
+++ src/signature.rs
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+//! Sign-to-Contract signatures
+
+use core::ffi::{c_int, c_uchar, c_uint, c_void};
+
+use secp256k1::ffi::secp256k1_nonce_function_rfc6979;
+
+use crate::hashes::TweakHash as _;
+
+pub struct NonceFnData<'s, C: secp256k1::Signing> {
+    pub ctx: &'s secp256k1::Secp256k1<C>,
+    pub algo: &'s str,
+    pub data: &'s [u8],
+    pub original_nonce: Option<secp256k1::PublicKey>,
+}
+
+// SAFETY:
+//   * The *32 arguments must be pointers to 32-byte array.
+//   * The `algo16` argument must be a pointer to a 16-byte array.
+//   * The `data` argument must be a pointer to a `NonceFnData`
+pub unsafe extern "C" fn s2c_ecdsa_nonce_fn<C: secp256k1::Signing>(
+    nonce32: *mut c_uchar,
+    msg32: *const c_uchar,
+    key32: *const c_uchar,
+    algo16: *const c_uchar,
+    data: *mut c_void,
+    attempt: c_uint,
+) -> c_int {
+    // 1. First compute original nonce using RFC6979. (This is not a visible
+    //    choice and we can change it later without breaking verifiers.)
+    let default_fn = secp256k1_nonce_function_rfc6979.unwrap(); // unwrap is because secp-sys API is dumb
+    if default_fn(nonce32, msg32, key32, algo16, data, attempt) == 0 {
+        return 0;
+    }
+    let data = data.cast::<NonceFnData<'_, C>>().as_mut().expect("required by SAFETY comment");
+
+    // 2. The returned nonce is a secret nonce. Interpret it as a secret key
+    //    and tweak it.
+    let nonce32 = nonce32.cast::<[u8; 32]>().as_mut().expect("required by SAFETY comment");
+    let Ok(sk) = secp256k1::SecretKey::from_byte_array(*nonce32)
+    else {
+        return 0
+    };
+
+    let untweaked_pubkey = sk.public_key(data.ctx);
+    let tweak_bytes = crate::Sign2ContractHash::compute_tweak(&untweaked_pubkey, data.algo, data.data).to_byte_array();
+    let tweak_sc = secp256k1::Scalar::from_be_bytes(tweak_bytes).expect("cryptographically unreachable");
+    let tweaked_sk = sk.add_tweak(&tweak_sc).expect("cryptographically unreachable");
+
+    // 3. Put the tweaked nonce in the outptr, and untweaked nonce in the data carrier.
+    nonce32.copy_from_slice(&tweaked_sk.secret_bytes());
+    data.original_nonce = Some(untweaked_pubkey);
+
+    return 1;
+}
